deebf3ace016b4119630e87c00432fec1e660aad
Partha Sarathi Kar <pskpartha@Parthas-MacBook-Pro.local>
2020-02-02 19:40:31 +0200

    bootstrap package changed

--- mapapp/src/app/app-routing.module.ts
+++ mapapp/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'header',
+  //   loadChildren: () =>
+  //     import('./header/header.module').then(m => m.HeaderModule)
+  // },
+  // {
+  //   path: 'footer',
+  //   loadChildren: () =>
+  //     import('./footer/footer.module').then(m => m.FooterModule)
+  // },
+  // {
+  //   path: 'map',
+  //   loadChildren: () => import('./map/map.module').then(m => m.MapModule)
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- mapapp/src/app/app.module.ts
+++ mapapp/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  declarations: [AppComponent, HeaderComponent, MapComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
